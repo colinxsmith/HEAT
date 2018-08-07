b6eeabbfcd2cd18afe6e421e14e62200a171c8d0
Colin Smith <colin.smith@corfinancialgroup.com>
2018-08-07 16:34:20 +0100

    Tweens

--- src/app/heatmap/heatmap.component.ts
+++ src/app/heatmap/heatmap.component.ts
@@ -3,7 +3,6 @@ import * as d3 from 'd3';
 // import { RGBColor } from 'd3';
 import { DatamoduleModule } from '../datamodule/datamodule.module';
 import { AppComponent } from '../app.component';
-import { easeBounce } from 'd3';
 @Component({
   selector: 'app-heatmap',
   // tslint:disable-next-line:max-line-length
@@ -204,7 +203,6 @@ export class HeatmapComponent implements OnInit {
             datamag
               .attr('class', 'mag')
               .attr('width', 0)
-              .attr('height', 0)
               .attr('x', (d) => d3.select(d.nodes()[(i + 1) % d.nodes().length]).attr('x'))
               .attr('y', 4)
               .style('fill', 'rgb(5, 247, 236)')
@@ -218,9 +216,10 @@ export class HeatmapComponent implements OnInit {
               )
               .on('mouseout', () => this.tooltip.style('opacity', 0));
             datamag.transition().duration(500)
+              .styleTween('opacity', () => (t: number) => `${t * t}`)
               .style('fill', (d) => d3.select(d.nodes()[i]).style('fill'))
               .attr('x', (d) => d3.select(d.nodes()[i]).attr('x'))
-              .attr('height', heightHere)
+              .attrTween('height', () => (t: number) => `${heightHere * t * t}px`)
               .attr('width', (d) => d3.select(d.nodes()[i]).attr('width'));
             datamagLab
               .attr('x', 0)
@@ -231,11 +230,10 @@ export class HeatmapComponent implements OnInit {
                 return `translate(${dt} , ${heightHere / 2}) rotate(270)`;
               }
               )
-              .style('opacity', 0)
               .attr('class', 'totalsX')
               .text((d, id) => `${this.managerData[id][i].value}`)
               .transition().duration(1500)
-              .style('opacity', 1)
+              .styleTween('opacity', () => (t: number) => `${Math.sqrt(t)}`)
               .attr('transform', (d) => {
                 const dt = +d3.select(d.nodes()[i]).attr('x').replace('px', '') +
                   +d3.select(d.nodes()[i]).attr('width').replace('px', '') / 2;
