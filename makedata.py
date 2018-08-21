93efe562b3c97aa71e093c7febcdfef887edf4b9
Colin Smith <colin.smith@corfinancialgroup.com>
2018-08-21 10:31:23 +0100

    Add more perf data

--- makedata.py
+++ makedata.py
@@ -1,12 +1,12 @@
 from random import random
-line = ''
+line = 'hold: [ '
 for i in range(150):
 	if random() > 0.5:
 		line += 'true,'
 	else:
 		line += 'false,'
-print line
-line = ''
+print line + ' ],'
+line = 'performance: [ '
 for i in range(150):
 	line += '%7.5f,' % (random() - 0.5)
-print line
+print line + ' ],'
