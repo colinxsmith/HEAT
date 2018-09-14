dde4b5435705e8b511785aec8caebd8ac523cb66
Colin Smith <colin.smith@corfinancialgroup.com>
2018-09-14 17:40:54 +0100

    more true block in data

--- makedata.py
+++ makedata.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # pipe to | sed "s/, \]/]/"
+keep=0
 from random import random
 line = ",{name: 'Vodafone Group',hold: ["
 for i in range(150):
-	if random() > 0.5:
+        keep+=1
+	if random() > 0.5 or keep < 7:
 		line += 'true,'
 	else:
 		line += 'false,'
+        if keep == 20:keep = 0
 print line + ' ],'
 line = 'performance: ['
 for i in range(150):
