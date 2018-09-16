816d3eaf8590129772cda92719970f8ba4774532
Colin <colinxsmith@gmail.com>
2018-09-16 17:22:37 +0100

    better perf map data

--- makedata.py
+++ makedata.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 # pipe to | sed "s/, \]/]/"
 keep=0
+state='true,'
 from random import random
 line = ",{name: 'Vodafone Group',hold: ["
 for i in range(150):
         keep+=1
-	if random() > 0.5 or keep < 7:
-		line += 'true,'
-	else:
-		line += 'false,'
-        if keep == 20:keep = 0
+	line += state
+        if keep >= 15:
+            if random() > 0.5:
+                keep = 0
+                if state is 'true,':
+                    state = 'false,'
+                else:
+                    state = 'true,'
 print line + ' ],'
 line = 'performance: ['
 for i in range(150):
