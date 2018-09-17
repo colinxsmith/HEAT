3e7637278bed91c3e69451caf297f7b7fc55f2c6
Colin Smith <colin.smith@corfinancialgroup.com>
2018-09-17 10:02:01 +0100

    update

--- makedata.py
+++ makedata.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 # pipe to | sed "s/, \]/]/"
-keep=0
-state='true,'
 from random import random
+keep = 0
+if random() > 0.5:
+    state = 'true,'
+else:
+    state = 'false,'
 line = ",{name: 'Vodafone Group',hold: ["
 for i in range(150):
-        keep+=1
-	line += state
-        if keep >= 15:
-            if random() > 0.5:
-                keep = 0
-                if state is 'true,':
-                    state = 'false,'
-                else:
-                    state = 'true,'
+    keep += 1
+    line += state
+    if keep >= 15:
+        if random() > 0.5:
+            keep = 0
+            if state is 'true,':
+                state = 'false,'
+            else:
+                state = 'true,'
 print line + ' ],'
 line = 'performance: ['
 for i in range(150):
